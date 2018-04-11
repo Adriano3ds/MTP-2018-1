b12b876f5102b74ca72f178661803c41464d1723
Adriano Junior <jr.adrianogm@gmail.com>
2018-04-11 09:54:57 -0300

    Update P1.c

--- Aula3/P1.c
+++ Aula3/P1.c
@@ -35,6 +35,6 @@ int main(){
         }
         i+=1;
     }
-    printf("A sequencia %seh multipla de 3", (estado?"nao ":""));
+    printf("Estado: %d\nA sequencia %s %seh multipla de 3", estado, bits, (estado?"nao ":""));
     return 0;
 }
