0401e10beaf831655dfd6af342deee60670c4cd7
Adriano Junior <jr.adrianogm@gmail.com>
2018-04-23 23:12:01 -0300

    P2.c upload

---
+++ Aula4/P2.c
@@ -0,0 +1,137 @@
+#define ALMOSTZERO 0.0000000000000000001
+#include <stdio.h>
+
+char l[17] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+int getValue(char c){
+	int i;
+    if(c>='a' && c<='f')
+        c-=32;
+    for(i=0; l[i]!=c; i++);
+    return i;
+}
+
+int strsize(char *str){
+	int i;
+    for(i=0; str[i]!='\0'; i++);
+    return i;
+}
+
+int mpow(int base, int e){
+    int res = 1;
+    for(; e>0; res*=base, e--);
+    return res;
+}
+
+double ln(double x){
+    double sum = 0.0, xmlxpl = (x - 1) / (x + 1), denom = 1.0,frac = xmlxpl, term = frac / denom;
+    while (term > ALMOSTZERO){
+        sum += term;
+        denom += 2.0;
+        frac = frac * xmlxpl * xmlxpl;
+        term = frac / denom;
+    }
+    return 2.0 * sum;
+}
+
+double logbase(double base, double x){
+    return ln(x)/ln(base);
+}
+
+int baseToDec(int base, char *val){
+    int len = strsize(val) -1, res=0, i;
+    for(i=len; i>=0; i--){
+        res += getValue(val[i])*mpow(base, (len-i));
+    }
+    return res;
+}
+
+void decToBase(int dec, int base){
+    int r, size = (int)logbase(base, dec)+1, i;
+    char res[size+1], tmp[size+1];
+	for(i=0; i<size; tmp[i]='0', i++);
+	i=0;
+    while(dec != 0){
+        r = dec%base;
+        dec = dec/base;
+        tmp[i++] = l[r];
+    }
+	for(i=size; i>=0; i--){
+		res[size-i] = tmp[i-1];
+	}
+    res[size] = '\0';
+	printf("%s", res);
+}
+
+int main(){
+    char inpt[256];
+    int opt = -1, exit=0, auxi;
+    while(exit==0){
+        while(!(opt>=1 && opt<=9)){
+            printf("\n\n=-=-=-=-=-=-=Menu=-=-=-=-=-=-=\n");
+            printf("1 - Bin para Dec\n2 - Bin para Hex\n3 - Hex para Dec\n4 - Hex para Bin\n5 - Dec para Bin\n6 - Dec para Hex\n7 - Oct para Dec\n8 - Dec para Oct\n9 - Sair");
+            printf("\n\nEscolha a opcao> ");
+            scanf("%d", &opt);
+			getchar();
+        }
+        switch(opt){
+            case 1:
+                printf("\nDigite o valor em binario(sem espacos): ");
+                scanf("%s", &inpt);
+                printf("%d", baseToDec(2, inpt));
+                break;
+            case 2:
+                printf("Digite o valor em binario(sem espacos): ");
+				scanf("%s", &inpt);
+				auxi = baseToDec(2, inpt);
+				printf("%d\n", auxi);
+				decToBase(auxi, 16);
+                break;
+            case 3:
+				printf("\nDigite o valor em hexadecimal(sem usar 0x no comeco): ");
+                scanf("%s", &inpt);
+                printf("%d", baseToDec(16, inpt));
+                break;
+            case 4:
+				printf("\nDigite o valor em hexadecimal(sem usar 0x no comeco): ");
+				scanf("%s", &inpt);
+				auxi = baseToDec(16, inpt);
+				decToBase(auxi, 2);
+                break;
+            case 5:
+				printf("\nDigite o valor em decimal: ");
+				scanf("%s", &inpt);
+				auxi = baseToDec(10, inpt);
+				decToBase(auxi, 2);
+                break;
+            case 6:
+				printf("\nDigite o valor em decimal: ");
+				scanf("%s", &inpt);
+				auxi = baseToDec(10, inpt);
+				decToBase(auxi, 16);
+                break;
+            case 7:
+				printf("\nDigite o valor em octal: ");
+				scanf("%s", &inpt);
+				printf("%d", baseToDec(8, inpt));
+                break;
+            case 8:
+				printf("\nDigite o valor em decimal: ");
+				scanf("%s", &inpt);
+				auxi = baseToDec(10, inpt);
+				decToBase(auxi, 8);
+                break;
+            case 9:
+                exit = 1;
+                break;
+        }
+		if(opt!=9){
+			printf("\nPrecione uma tecla para continuar...");
+			getchar();
+			getchar();
+		}
+		opt = -1;
+    }
+    printf("\nAte mais!");
+    return 0;
+}
