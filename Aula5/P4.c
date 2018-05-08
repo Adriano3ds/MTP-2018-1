298470d2cf84711790ed7b20eab4d315950c4c17
Adriano Junior <jr.adrianogm@gmail.com>
2018-05-07 22:35:00 -0300

    P3 e P4 adicionados

---
+++ Aula5/P4.c
@@ -0,0 +1,20 @@
+/*
+ *  Adriano Junior
+ *   11721EBI010
+ */
+ 
+#include <stdio.h>
+
+int main(){
+	char num[256];
+	int n = 0, i;
+	printf("Digite uma string: ");
+	scanf("%s", &num);
+	for(i=0; num[i]!='\0'; i++){
+		if(num[i] >= '0' && num[i]<= '9'){
+			n = n*10 + (num[i] - '0');
+		}
+	}
+	printf("\n%d\n", n);
+	return 0;
+}
