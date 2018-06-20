1f5c08aa97dcc1ec6f2d1ea04919784e768bc19f
Adriano Junior <jr.adrianogm@gmail.com>
2018-06-20 20:10:40 -0300

    Fixed P4.c

--- Aula5/P4.c
+++ Aula5/P4.c
@@ -9,7 +9,7 @@ int main(){
 	char num[256];
 	int n = 0, i;
 	printf("Digite uma string: ");
-	scanf("%s", &num);
+	scanf("%s", (char*) &num);
 	for(i=0; num[i]!='\0'; i++){
 		if(num[i] >= '0' && num[i]<= '9'){
 			n = n*10 + (num[i] - '0');
